9fcb049e74a0d150b8dd4ace305fb6c380c47c67
Henri Heimann <henriheimann@gmail.com>
2021-12-11 16:01:52 +0100

    Add sensor nodes hardware files and irrigation sensor embedded project

---
+++ embedded-sw/irrigation-sensor/Core/Inc/main.h
@@ -0,0 +1,78 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32l4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define VIN_SENSE_Pin GPIO_PIN_0
+#define VIN_SENSE_GPIO_Port GPIOA
+#define RFM95_NRST_Pin GPIO_PIN_3
+#define RFM95_NRST_GPIO_Port GPIOA
+#define RFM95_NSS_Pin GPIO_PIN_4
+#define RFM95_NSS_GPIO_Port GPIOA
+#define RFM95_IRQ_Pin GPIO_PIN_1
+#define RFM95_IRQ_GPIO_Port GPIOB
+#define I2C_ENABLE_Pin GPIO_PIN_8
+#define I2C_ENABLE_GPIO_Port GPIOA
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
