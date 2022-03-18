890f457cfacec4b7924e7a425abe51a95268f900
Henri Heimann <henriheimann@gmail.com>
2022-03-18 13:19:36 +0100

    Try fixing e2e docker env build variables

--- deployment/p3m-local-e2e/deploy-local-e2e.sh
+++ deployment/p3m-local-e2e/deploy-local-e2e.sh
@@ -9,7 +9,7 @@ source <(grep -v -e '^#\|^[[:space:]]*$' .env-secrets | sed -e 's/\r$//' -e 's/^
 
 # build frontend image
 cd ../../frontend/
-docker build -t p3m-frontend . \
+docker buildx build --platform=linux/amd64 -t p3m-frontend . \
   --build-arg frontend_production=true \
   --build-arg deployment_url="${DEPLOYMENT_URL}" \
   --build-arg backend_url="${DEPLOYMENT_URL}/api" \
@@ -19,7 +19,7 @@ cd ../deployment/p3m-local-e2e/
 # build backend image
 cd ../../backend/
 mvn clean package -DskipTests
-docker build -t p3m-backend .
+docker buildx build --platform=linux/amd64 -t p3m-backend .
 cd ../deployment/p3m-local-e2e/
 
 # run compose
